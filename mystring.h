223449914c4c6f444bbf1fc4670dd1347dd9f810
Panagiotis_INS <39010461+Panagiotis-INS@users.noreply.github.com>
2020-09-16 20:21:20 +0300

    Add files via upload

---
+++ mystring.h
@@ -0,0 +1,26 @@
+#ifndef stringm
+#define stringm
+#include<string.h>
+#include<iostream>
+using namespace std;
+class mstring{
+private:
+	char* ptr;
+	int size;
+public:
+	char* c_str();
+	int length();
+	//
+	mstring& operator+=(char*);//done
+	mstring& operator+=(char);//done
+	mstring& operator+=(mstring&);//done
+	mstring& operator=(mstring&);//done
+	mstring& operator=(char*);//done
+	mstring& operator=(char);//done
+	//
+	mstring();
+	mstring(char*);
+	~mstring();
+};
+ostream& operator<<(ostream&,mstring&);
+#endif
